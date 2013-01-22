51f74f90e241cf5cae601e15416be227a51ee32c
Stefanie Botelho <stefanie.botelho@gmail.com>
2013-01-21 21:02:59 -0500

    Cart Creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  
+  # Get the list of products out of the database
+  # And make it available to the code in the view that will display the table 
   def index
   	@products = Product.order(:title)
   end
